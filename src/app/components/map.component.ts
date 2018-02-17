fcbfd5173b17c2674a567862ec75c186c284cd51
bciancio <Brandonciancio@gmail.com>
2018-02-17 16:14:43 -0600

    Map setup

--- src/app/components/map.component.ts
+++ src/app/components/map.component.ts
@@ -4,14 +4,23 @@ import { log } from 'util';
 import { MouseEvent } from '@agm/core';
 import { Marker } from '../models/marker';
 import { MarkerService } from '../services/markers.service';
+import { MapOptions } from '@agm/core/services/google-maps-types';
 
 @Component({
     selector: 'app-root',
     templateUrl: '../pages/map.component.html',
-    styleUrls: ['../pages/map.component.html', '../../assets/materialize/css/materialize.min.css'],
+    styleUrls: ['../pages/map.component.css', '../../assets/materialize/css/materialize.min.css'],
     providers: [MarkerService]
 })
 export class MapComponent {
+    map_options: MapOptions = {
+        zoom: 12,
+        center: {
+            lat: 43.0730520,
+            lng: -89.4012300,
+        }        
+    }
+
     markers: Marker[];
     selected_marker: Marker;
 
